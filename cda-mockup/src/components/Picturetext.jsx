d5689499e296745bd534bb95edab4c6af3a90f91
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2020-05-01 10:56:39 -0400

    deploying

--- cda-mockup/src/components/Picturetext.jsx
+++ cda-mockup/src/components/Picturetext.jsx
@@ -4,14 +4,20 @@ import Headimage from './assets/cda-interview-guide.jpg'
 export default function Picturetext () {
 return (
 
-<div className = "pictureText-container">
+<div className = "pictureText-container"
+style={{width:"100%"}}>
 
 <div className="pictureText-Text">
 <h1>CDA Interview Guide</h1>
 
-<hr />
+
 </div>
 
+<div className="underline"
+style={{ width:"40%"}}>
+<hr />
+
+</div>
 
 
 </div>
